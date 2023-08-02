d92d47afd8a639d35471117d4710917be4e75344
Tranhuy1612 <128037186+Tranhuy1612@users.noreply.github.com>
2023-08-02 11:32:09 +0700

    Add files via upload

---
+++ ThucHanhss10-3/src/main/java/com/example/thuchanhss103/ConverterServlet.java
@@ -0,0 +1,32 @@
+package com.example.thuchanhss103;
+
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/convert")
+public class ConverterServlet extends HttpServlet {
+    private String message;
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, IOException {
+        float rate = Float.parseFloat(request.getParameter("rate"));
+        float usd = Float.parseFloat(request.getParameter("usd"));
+        float vnd = rate * usd;
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Rate: " + rate + "</h1>");
+        writer.println("<h1>USD: " + usd  + " $" + "</h1>");
+        writer.println("<h1>VND: " + vnd + " VND"+"</h1>");
+        writer.println("</html>");
+
+    }
+
+    public void destroy() {
+    }
+}
---
+++ ThucHanhss10/src/main/java/com/example/thuchanhss10/ConverterServlet.java
@@ -0,0 +1,36 @@
+package com.example.thuchanhss10;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+import java.io.*;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/time-world")
+public class ConverterServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        //Hello
+        writer.println("<html><body>");
+        writer.println("<h1>" + message + "</h1>");
+        writer.println("</body></html>");
+        //Date time
+        writer.println("<html>");
+        Date today = new Date();
+        writer.println("<h1>" + today + "</h1>");
+        writer.println("</html>");
+    }
+
+    public void destroy() {
+    }
+}
